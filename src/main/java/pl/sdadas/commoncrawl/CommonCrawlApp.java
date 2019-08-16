57fc9168b1b196bbcf3012dc90c986f058ed1f01
Sławomir Dadas <sdadas@opi.org.pl>
2019-08-16 14:33:57 +0200

    Passing number of threads as a parameter

--- src/main/java/pl/sdadas/commoncrawl/CommonCrawlApp.java
+++ src/main/java/pl/sdadas/commoncrawl/CommonCrawlApp.java
@@ -22,15 +22,19 @@ public class CommonCrawlApp {
 
 
     public static void main(String[] args) throws Exception {
+        if(args.length < 2) {
+            LOG.info("usage: java -jar commoncrawl-downloader.jar [directory:str] [num_threads:int] [lang_byte_limit:int]");
+            System.exit(0);
+        }
         File baseDir = new File(args[0]);
         File wetPaths = new File(baseDir, "wet.paths");
         File tempDir = new File(baseDir, "temp");
         File filteredDir = new File(baseDir, "filtered");
-        ExecutorService service = Executors.newFixedThreadPool(4);
+        ExecutorService service = Executors.newFixedThreadPool(Integer.parseInt(args[1]));
         FileUtils.forceMkdir(filteredDir);
         LineIterator iter = FileUtils.lineIterator(wetPaths, "UTF-8");
         FastTextLangDetector ld = new FastTextLangDetector();
-        long limit = args.length > 1 ? Long.parseLong(args[1]) : MultiLangCounter.MAX_SIZE;
+        long limit = args.length > 2 ? Long.parseLong(args[2]) : MultiLangCounter.MAX_SIZE;
         MultiLangCounter counter = new MultiLangCounter(filteredDir, limit);
         LOG.info(counter.state());
         while(iter.hasNext()) {
